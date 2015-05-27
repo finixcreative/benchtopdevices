87ba6140322f57ada7840883985a6d4cc599d59b
finix-rsmith <rsmith@finixcreative.com>
2015-05-27 15:32:11 -0400

    Modules - directive syntax
    Add missing commas to fix directives

--- app/modules/contact/contact-drv.js
+++ app/modules/contact/contact-drv.js
@@ -2,7 +2,7 @@ angular.module('coreMod')
 	.directive('contact', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/benchtopdevices/app/modules/contact/contact.html'
+			templateUrl: '/benchtopdevices/app/modules/contact/contact.html',
 			controller: 'ClientController',
 			controllerAs: 'client',
 		};
--- app/modules/nav/nav-drv.js
+++ app/modules/nav/nav-drv.js
@@ -2,7 +2,7 @@ angular.module('coreMod')
 	.directive('navigation', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/benchtopdevices/app/modules/nav/nav.html'
+			templateUrl: '/benchtopdevices/app/modules/nav/nav.html',
 			controller: 'ClientController',
 			controllerAs: 'client',
 		};
