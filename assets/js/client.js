4220a0a31124293a10a6dae4060ef827cadd71c7
finix-rsmith <rsmith@finixcreative.com>
2015-05-27 14:25:40 -0400

    Client - controller and data
    Add client scripts

---
+++ assets/js/client.js
@@ -0,0 +1,53 @@
+var client = {
+	'company': '',
+	'contact': {
+		'email': '',
+		'phone': '',
+		'fax': '',
+	},
+	'locations': [
+		{
+			'address': {
+				'street': '',
+				'city': '',
+				'state': '',
+				'zip': '',
+			},
+			'map': '',
+		},
+	],
+	'socials': [
+		{
+			'name': 'facebook',
+			'link': '',
+		},
+		{
+			'name': 'twitter',
+			'link': '',
+		},
+		{
+			'name': 'linkedin',
+			'link': '',
+		},
+		{
+			'name': 'google+',
+			'link': '',
+		},
+		{
+			'name': 'youtube',
+			'link': '',
+		},
+		{
+			'name': 'instagram',
+			'link': '',
+		},
+		{
+			'name': 'pinterest',
+			'link': '',
+		},
+		{
+			'name': 'tumblr',
+			'link': '',
+		},
+	],
+};
