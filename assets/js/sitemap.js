a992e37aafdfb52cfad7e17defdc823ced08cd6b
finix-rsmith <rsmith@finixcreative.com>
2015-06-04 12:44:13 -0400

    Sitemap - controller & data
    Add sitemap controller for structure and content management

---
+++ assets/js/sitemap.js
@@ -0,0 +1,68 @@
+var sitemap = {
+	pages: [
+		{
+			title: 'home',
+			logo: '',
+			background: '',
+			headline: '',
+			subheadline: '',
+			text: '',
+			subsections: [
+				{
+					headline: '',
+					subheadline: '',
+					text: '',
+					size: '',
+				},
+			],
+		},
+		{
+			title: 'services',
+			logo: '',
+			background: '',
+			headline: '',
+			subheadline: '',
+			text: '',
+			subsections: [
+				{
+					headline: '',
+					subheadline: '',
+					text: '',
+					size: '',
+				},
+			],
+		},
+		{
+			title: 'technical',
+			logo: '',
+			background: '',
+			headline: '',
+			subheadline: '',
+			text: '',
+			subsections: [
+				{
+					headline: '',
+					subheadline: '',
+					text: '',
+					size: '',
+				},
+			],
+		},
+		{
+			title: 'about',
+			logo: '',
+			background: '',
+			headline: '',
+			subheadline: '',
+			text: '',
+			subsections: [
+				{
+					headline: '',
+					subheadline: '',
+					text: '',
+					size: '',
+				},
+			],
+		},
+	],
+};
