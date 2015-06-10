df561c538a3868b4cb27783d86f62675f1fd7891
finix-rsmith <rsmith@finixcreative.com>
2015-06-09 20:38:41 -0400

    Carousel - new slide
    Add third slide to test transitions

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -35,35 +35,15 @@ var images = [
 			},
 		],
 	},
-];
-var images2 = [
 	{
 		"background": {
-			"img": "assets/images/istock/zoom-40952736-3.jpg",
+			"img": "assets/images/istock/zoom-12299016-3.jpg",
 			"color": "#",
 			"position": "center",
 		},
 		"id": "slide1",
 		"state": "active",
-		"headline": "Slides 2.1",
-		"subheadline": "Subheadline",
-		"text": "Lorem Ipsum",
-		"links": [
-			{
-				"text": "Get service >",
-				"url": "#/services",
-			},
-		],
-	},
-	{
-		"background": {
-			"img": "assets/images/istock/zoom-39527184-3.jpg",
-			"color": "#",
-			"position": "center",
-		},
-		"id": "slide2",
-		"state": "",
-		"headline": "Slides 2.2",
+		"headline": "Slide 3",
 		"subheadline": "Subheadline",
 		"text": "Lorem Ipsum",
 		"links": [
