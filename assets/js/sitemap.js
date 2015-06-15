474b10c1f072242ae06576cb8dcd286c97530774
finix-rsmith <rsmith@finixcreative.com>
2015-06-15 01:00:27 -0400

    Home - directives & data
    Refactor homepage to use directives and update sitemap

--- assets/js/sitemap.js
+++ assets/js/sitemap.js
@@ -1,18 +1,52 @@
 var sitemap = {
 	pages: [
 		{
-			title: 'home',
-			logo: '',
-			background: '',
-			headline: '',
-			subheadline: '',
-			text: '',
-			subsections: [
+			rows: [
 				{
-					headline: '',
-					subheadline: '',
-					text: '',
-					size: '',
+					classes: 'row full',
+					subsections: [
+						{
+							title: 'Fixture design, custom testers, & contract machining',
+							subtitle: '',
+							classes: 'subsection-major sublink content full',
+							link: '#/services',
+							image: {
+								src: 'assets/images/repair.jpg',
+								alt: '',
+								classes: 'full',
+							},
+							text: 'We offer leak detection and location services to customers in the New England region.',
+						},
+					],
+				},
+				{
+					classes: 'row alt full',
+					subsections: [
+						{
+							title: 'Technical Information',
+							subtitle: '',
+							classes: 'subsection-minor half content center sublink',
+							link: '#/technical',
+							image: {
+								src: '',
+								alt: '',
+								classes: '',
+							},
+							text: 'Learn more about leak detection and location.',
+						},
+						{
+							title: 'About Us',
+							subtitle: '',
+							classes: 'subsection-minor half content center sublink',
+							link: '#/about',
+							image: {
+								src: '',
+								alt: '',
+								classes: '',
+							},
+							text: 'Learn more about our company.',
+						},
+					],
 				},
 			],
 		},
