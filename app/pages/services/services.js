50f1721c51f72c018f041beaa2a5fe50baae323b
finix-rsmith <rsmith@finixcreative.com>
2015-06-18 18:13:14 -0400

    Services - images
    Add images to services page

--- app/pages/services/services.js
+++ app/pages/services/services.js
@@ -1,14 +1,14 @@
 var services = [
 	{
 		title: 'Leak Detection',
-		image: 'assets/images/.jpg',
-		cost: '$200',
+		image: 'assets/images/istock/zoom-40952736-3.jpg',
+		cost: '$Price',
 		size: 'half',
 	},
 	{
 		title: 'Leak Location',
-		image: 'assets/images/.jpg',
-		cost: '$150',
+		image: 'assets/images/istock/zoom-39527184-3.jpg',
+		cost: '$Price',
 		size: 'half',
 	},
 ];
