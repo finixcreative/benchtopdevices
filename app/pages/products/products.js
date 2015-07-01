1a324f92377046f3ab61f6b3fa2e62898ad15a17
finix-rsmith <rsmith@finixcreative.com>
2015-07-01 10:55:51 -0400

    Products - database
    Add three more products to the database

--- app/pages/products/products.js
+++ app/pages/products/products.js
@@ -29,4 +29,34 @@ var products = [
 		},
 		link: 'http://www.finixcreative.com',
 	},
+	{
+		name: 'Industries Served',
+		size: 'third',
+		classes: 'tile content',
+		img: {
+			src: 'assets/images/istock/zoom-40952736-3.jpg',
+			alt: 'product 1'
+		},
+		link: 'http://www.finixcreative.com',
+	},
+	{
+		name: 'Resources',
+		size: 'third',
+		classes: 'tile content',
+		img: {
+			src: 'assets/images/istock/zoom-40952736-3.jpg',
+			alt: 'product 1'
+		},
+		link: 'http://www.finixcreative.com',
+	},
+	{
+		name: 'Check it out!',
+		size: 'third',
+		classes: 'tile content',
+		img: {
+			src: 'assets/images/istock/zoom-40952736-3.jpg',
+			alt: 'product 1'
+		},
+		link: 'http://www.finixcreative.com',
+	},
 ];
