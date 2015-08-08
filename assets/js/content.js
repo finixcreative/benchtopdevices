7ae3b1e6d9885f65f44ff33db7727bf49faf41cd
finix-rsmith <rsmith@finixcreative.com>
2015-08-08 01:10:29 -0400

    Home - inficon content
    Shorten inficon content to match cts

--- assets/js/content.js
+++ assets/js/content.js
@@ -38,7 +38,7 @@ var content = {
 				},
 				headline: 'Leak Location',
 				subheadline: 'Helium ~ Hydrogen',
-				text: 'Inficon is a leading provider of innovative instrumentation, critical sensor technologies, and advanced process control software that enhance productivity and quality in sophisticated industrial vacuum processes. These analysis, measurement and control products are essential for gas leak detection in air conditioning/refrigeration and automotive manufacturing. They are vital to equipment manufacturers and end-users in the complex fabrication of semiconductors and thin film coatings for optics, flat panel displays, solar cells and industrial vacuum coating applications. Other users of our vacuum-based processes include the life sciences, research, aerospace, packaging, heat treatment, laser cutting and many other industrial processes. We also leverage our expertise in vacuum technology to provide unique, toxic chemical analysis products for emergency response, security, and environmental health and safety.',
+				text: 'Inficon is a leading provider of innovative instrumentation, critical sensor technologies, and advanced process control software that enhance productivity and quality in sophisticated industrial vacuum processes. These analysis, measurement and control products are essential for gas leak detection in air conditioning/refrigeration and automotive manufacturing.',
 				cost: '$Price',
 				image: {
 					src: 'assets/images/inficon/inficon-service.png',
