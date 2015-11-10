34b2c1788e8c164a724207e4e6696faf00fa92f7
rsmith-finix <rsmith@finixcreative.com>
2015-11-10 09:31:44 -0800

    Overrides - unit test
    Add console log note

--- assets/js/overrides.js
+++ assets/js/overrides.js
@@ -2,4 +2,7 @@
 var overrides = function(){
 	$(".call-now").empty().append("<a href='tel:1-603-256-6100'>Call 603-256-6100</a>");
 };
-overrides();
+window.onload = function(){
+	overrides();
+	console.log("Overrides applied");
+};
