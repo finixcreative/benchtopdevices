85b1bf71309f4136987aa2048f25910de81f7d3e
rsmith-finix <rsmith@finixcreative.com>
2015-11-10 09:22:11 -0800

    Overrides - update get request
    Change syntax to plural “elements”

--- assets/js/overrides.js
+++ assets/js/overrides.js
@@ -2,4 +2,4 @@
 var overrides = function(){
 	$(".call-now").empty().append("<a href='tel:1-603-256-6100'>Call 603-256-6100</a>");
 };
-document.getElementByClassName("call-now").onload = overrides();
+document.getElementsByClassName("call-now").onload = overrides();
