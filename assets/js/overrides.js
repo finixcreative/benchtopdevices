6ddec5b7fe21c03f7eab8d5257e7022e3a6dbb19
rsmith-finix <rsmith@finixcreative.com>
2015-11-09 21:46:43 -0800

    Overrides - variable
    Move function inside variable

--- assets/js/overrides.js
+++ assets/js/overrides.js
@@ -1,5 +1,5 @@
 /* Overrides */
-function overrides(){
+var overrides = function(){
 	$(".call-now").empty().append("<a href='tel:1-603-256-6100'>Call 603-256-6100</a>");
 };
 window.onload = overrides();
