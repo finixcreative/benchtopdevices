62002b7274b7c394d0714d5d953e4c4a6ce61e52
rsmith-finix <rsmith@finixcreative.com>
2015-11-11 09:00:58 -0800

    Overrides - for loop
    Use for loop instead of timeout to control number of repetitions

--- assets/js/overrides.js
+++ assets/js/overrides.js
@@ -1,9 +1,10 @@
 /* Overrides */
 var overrides = function(){
 	$(".call-now").empty().append("<a href='tel:1-603-256-6100'>Call <span class='desktop tablet'>603-256-6100</span></a>");
-	setTimeout(overrides, 100)
 };
 window.onload = function(){
-	overrides();
+	for (var i = 0; i < 5; i++) {
+		overrides();
+	};
 	console.log("Overrides applied: " + overrides);
 };
