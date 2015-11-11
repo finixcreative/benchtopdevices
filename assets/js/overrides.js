7f9c230899d323a111a3832984b423b67457805c
rsmith-finix <rsmith@finixcreative.com>
2015-11-11 09:04:46 -0800

    Overrides - use timeout
    Replace timeout function

--- assets/js/overrides.js
+++ assets/js/overrides.js
@@ -1,10 +1,9 @@
 /* Overrides */
 var overrides = function(){
-	$(".call-now").empty().append("<a href='tel:1-603-256-6100'>Call <span class='desktop tablet'>603-256-6100</span></a>");
+	$(".call-now").empty().append("<a href='tel:1-603-256-6100'>Call<span class='desktop tablet'> 603-256-6100</span></a>");
+	setTimeout(overrides, 100);
 };
 window.onload = function(){
-	for (var i = 0; i < 5; i++) {
-		overrides();
-	};
+	overrides();
 	console.log("Overrides applied: " + overrides);
 };
